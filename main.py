d254bd478faebb8d27599c4cfd57b11f60441e37
= <=>
2022-11-11 21:29:32 +0100

    remove test cube

--- main.py
+++ main.py
@@ -17,18 +17,6 @@ FG = (255, 255, 255)
 SIZE = 640, 480
 FPS = 60
 
-cubeVertices = ((1, 1, 1), (1, 1, -1), (1, -1, -1), (1, -1, 1), (-1, 1, 1), (-1, -1, -1), (-1, -1, 1), (-1, 1, -1))
-cubeEdges = ((0, 1), (0, 3), (0, 4), (1, 2), (1, 7), (2, 5), (2, 3), (3, 6), (4, 6), (4, 7), (5, 6), (5, 7))
-cubeQuads = ((0, 3, 6, 4), (2, 5, 6, 3), (1, 2, 5, 7), (1, 0, 4, 7), (7, 4, 6, 5), (2, 3, 0, 1))
-
-
-def wireCube():
-    glBegin(GL_LINES)
-    for cubeEdge in cubeEdges:
-        for cubeVertex in cubeEdge:
-            glVertex3fv(cubeVertices[cubeVertex])
-    glEnd()
-
 
 def start():
     # pygame opengl setup
@@ -96,7 +84,6 @@ def start():
         spawner.update(player, delta)
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        wireCube()
 
         # prepare to render the texture-mapped rectangle
         glClear(GL_COLOR_BUFFER_BIT)
