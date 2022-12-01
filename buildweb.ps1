91ee37cc8b2b03245935ffb237edf4bdfd614a44
brambasiel <bramtech@telenet.be>
2022-12-01 19:58:25 +0100

    online scoreboard won't work

--- buildweb.ps1
+++ buildweb.ps1
@@ -2,4 +2,4 @@
 
 # https://github.com/pygame-web/pygbag/blob/main/README.md
 pip3 install pygbag --upgrade
-python3 -m pygbag --app_name "Blasteroids" --build .
+python3 -m pygbag --app_name "Blasteroids" --build --html .
